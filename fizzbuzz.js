7fd32a3878f89c417229c29ac43df272770340a8
skyland31 <suttipong.kay.10@gmail.com>
2019-09-21 16:00:38 +0700

    if-else

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,12 @@
 class FizzBuzz {
   static say(number) {
+    if (number % 3 == 0 && number % 5 == 0) {
+      return "FizzBuzz";
+    } else if (number % 5 == 0) {
+      return "Buzz";
+    } else if (number % 3 == 0) {
+      return "Fizz";
+    }
     return 1;
   }
 }
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -6,3 +6,27 @@ describe("Test FizzBuzz", () => {
     expect(result).toEqual(1); //Assert Function
   });
 });
+describe("Test FizzBuzz", () => {
+  test("Test 2 expect 2", () => {
+    const result = FizzBuzz.say(2);
+    expect(result).toEqual(2); //Assert Function
+  });
+});
+describe("Test FizzBuzz", () => {
+  test("Test 3 expect Fizz", () => {
+    const result = FizzBuzz.say(3);
+    expect(result).toEqual("Fizz"); //Assert Function
+  });
+});
+describe("Test FizzBuzz", () => {
+  test("Test 5 expect Buzz", () => {
+    const result = FizzBuzz.say(5);
+    expect(result).toEqual("Buzz"); //Assert Function
+  });
+});
+describe("Test FizzBuzz", () => {
+  test("Test 15 expect FizzBuzz", () => {
+    const result = FizzBuzz.say(15);
+    expect(result).toEqual("FizzBuzz"); //Assert Function
+  });
+});
