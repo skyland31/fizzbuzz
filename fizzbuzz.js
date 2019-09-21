981a4ed50409167356b0ab65adf51d8c82c5caa0
skyland31 <suttipong.kay.10@gmail.com>
2019-09-21 16:11:21 +0700

    SwitchCase

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,22 @@
 class FizzBuzz {
   static say(number) {
-    if (number % 3 == 0 && number % 5 == 0) {
-      return "FizzBuzz";
-    } else if (number % 5 == 0) {
-      return "Buzz";
-    } else if (number % 3 == 0) {
-      return "Fizz";
+    // if (number % 3 == 0 && number % 5 == 0) {
+    //   return "FizzBuzz";
+    // } else if (number % 5 == 0) {
+    //   return "Buzz";
+    // } else if (number % 3 == 0) {
+    //   return "Fizz";
+    // }
+    switch (true) {
+      case number % 3 === 0 && number % 5 === 0:
+        return "FizzBuzz";
+      case number % 5 === 0:
+        return "Buzz";
+      case number % 3 === 0:
+        return "Fizz";
+      default:
+        return number;
     }
-    return number;
   }
 }
 module.exports = FizzBuzz;
